44eca3781e4f5af416324f6bd5a971438228bd4c
Michael Hedaitulla <mhedaitulla@users.noreply.github.com>
2019-07-05 17:35:39 -0400

    Creatd other code generators

--- SwiftFonts/Sources/CodeGen/DownloadableFontsCodeGenerator.swift
+++ SwiftFonts/Sources/CodeGen/DownloadableFontsCodeGenerator.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+/// Downloadable  iOS font code generator
+internal class DownloadableFontsCodeGenerator: CodeGenerator {
+    
+    public static let shared = DownloadableFontsCodeGenerator()
+    
+
+
+}
--- SwiftFonts/Sources/CodeGen/GoogleFontsCodeGenerator.swift
+++ SwiftFonts/Sources/CodeGen/GoogleFontsCodeGenerator.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+/// Preinstalled  iOS font code generator
+internal class GoogleFontsCodeGenerator: CodeGenerator {
+    
+    public static let shared = GoogleFontsCodeGenerator()
+
+
+}
