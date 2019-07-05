3fed30579290ed1d3d414c86e5a81404258d044f
Michael Hedaitulla <mhedaitulla@users.noreply.github.com>
2019-07-04 23:03:16 -0400

    Preped for DownloadableFonts and GoogleFonts

---
+++ SwiftFonts/Sources/CodeGen/GoogleFontsCodeGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleFontsCodeGenerator.swift
+//  SwiftFonts
+//
+//  Created by Michael Hedaitulla on 7/4/19.
+//  Copyright © 2019 Roadfire Software. All rights reserved.
+//
+
+import Foundation
